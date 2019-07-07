15664ca4beff0c94c2f5d3d7d7988d53db6de83a
scarsfun <pusillus@yahoo.com>
2019-07-07 15:05:44 +0200

    trs

--- RTE/_Target_1/RTE_Components.h
+++ RTE/_Target_1/RTE_Components.h
@@ -1,6 +1,6 @@
 
 /*
- * Auto generated Run-Time-Environment Component Configuration File
+ * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
  * Project: 'Lv_test' 
@@ -16,12 +16,19 @@
  */
 #define CMSIS_device_header "stm32f10x.h"
 
+/*  Keil.ARM Compiler::Compiler:I/O:STDOUT:ITM:1.2.0 */
 #define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
           #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
+/*  Keil::Device:StdPeriph Drivers:DMA:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_DMA
+/*  Keil::Device:StdPeriph Drivers:Framework:3.5.1 */
 #define RTE_DEVICE_STDPERIPH_FRAMEWORK
+/*  Keil::Device:StdPeriph Drivers:GPIO:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_GPIO
+/*  Keil::Device:StdPeriph Drivers:RCC:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_RCC
+/*  Keil::Device:StdPeriph Drivers:SPI:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_SPI
+
 
 #endif /* RTE_COMPONENTS_H */
