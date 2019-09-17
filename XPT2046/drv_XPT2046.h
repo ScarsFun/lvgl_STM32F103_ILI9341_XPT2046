6f7c6ccf0390b068f09c0b3e5566182d8b58a5b7
scarsfun <pusillus@yahoo.com>
2019-09-17 09:55:09 +0200

    delete

--- XPT2046/drv_XPT2046.h
+++
@@ -1,5 +0,0 @@
-uint8_t XPT2046_irq_read(void);
-void XPT2046_spi_cs_set(uint8_t val);
-void XPT2046_SPI2_init(void);
-uint8_t SPI_XchgByte(uint8_t byte);
-void XPT2046_GetTouch_XY( uint16_t *x_kor,uint16_t *y_kor, uint8_t count_read);
--- ili9341/lv_driver.h
+++
@@ -1,5 +0,0 @@
-#include "../lvgl/lvgl.h"
-#include "../lv_conf.h"
-
-void ILI9341_flush(struct _disp_drv_t *, const lv_area_t * , lv_color_t * );
-void ILI9341_flush(lv_disp_drv_t *, const lv_area_t * , lv_color_t * );
